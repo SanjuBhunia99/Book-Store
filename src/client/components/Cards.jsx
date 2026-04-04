78cdbf801dd6d7184df239b42f6b796665a7c0bf
Sanju Bhunia <sanjubhunia9@gmail.com>
2026-04-04 12:33:05 +0530

    Done

--- src/client/components/Cards.jsx
+++ src/client/components/Cards.jsx
@@ -1,56 +1,3 @@
-// import React from "react";
-// import { useCart } from "../context/CartContext";
-
-// const Cards = ({ item }) => {
-//   const { addToCart } = useCart();
-
-//   return (
-//     <div
-//       className="
-//         border rounded-lg shadow-md
-//         p-3 sm:p-4
-//         w-full
-//         transition-transform duration-300
-//         hover:scale-105
-//       "
-//     >
-//       <img
-//         src={item.image}
-//         alt={item.name}
-//         className="h-36 sm:h-40 md:h-44 w-full object-cover rounded"
-//       />
-//       <h2 className="mt-2 font-bold text-base sm:text-lg">{item.name}</h2>
-
-//       <p className="text-xs sm:text-sm text-gray-600">{item.title}</p>
-
-//       <p className="mt-1 font-semibold text-green-600 text-sm sm:text-base">
-//         ₹{item.price}
-//       </p>
-
-//       <span className="inline-block mt-2 text-xs bg-blue-100 px-2 py-1 rounded">
-//         {item.category}
-//       </span>
-//       <div className="mt-3">
-//         <button
-//           onClick={() => addToCart(item)}
-//           className="
-//             w-full
-//             bg-green-500 text-white
-//             py-2 text-sm sm:text-base
-//             rounded
-//             hover:bg-green-600
-//             transition
-//           "
-//         >
-//           Add to Cart
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Cards;
-
 import React from "react";
 import { useCart } from "../context/CartContext";
 
@@ -68,7 +15,6 @@ const Cards = ({ item }) => {
       <p className="text-sm text-gray-600">{item.title}</p>
       <p className="text-green-600 font-semibold">₹{item.price}</p>
 
-<<<<<<< HEAD
    
       <h2 className="mt-2 font-bold text-base sm:text-lg">{item.name}</h2>
 
@@ -97,14 +43,6 @@ const Cards = ({ item }) => {
           Add to Cart
         </button>
       </div>
-=======
-      <button
-        onClick={() => addToCart(item)}
-        className="w-full mt-3 bg-green-500 text-white py-2 rounded"
-      >
-        Add to Cart
-      </button>
->>>>>>> ac00a06c3f778acb5137f64b8541fad7accd2a9f
     </div>
   );
 };
